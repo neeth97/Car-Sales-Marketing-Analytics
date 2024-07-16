ac2704793d16f0a6059bb86ccae7188da2986bab
Praneeth Kruthiventi <100612144+neeth97@users.noreply.github.com>
2024-07-16 16:22:54 -0700

    Update functions_and_descriptions.py

--- functions_and_descriptions.py
+++ functions_and_descriptions.py
@@ -13,3 +13,38 @@ def create_new_columns_ad_channels():
     new_ad_channels_df['campaign'].fillna(new_ad_channels_df['partner'], inplace=True)
 
     return new_ad_channels_df
+# we identify the first touch for every user_id and store that in this dataframe.
+# first touch attribution a marketing technique where we care a lot about the
+# first channel an user interacts with first.
+
+def create_first_touch_df():
+    # sort all the clicks by the datetime in asc order
+    sorted_clicks_df = clicks.sort_values(by='click_datetime')
+    first_touch_df = sorted_clicks_df.drop_duplicates(subset=['user_id'], keep='first')
+
+    return first_touch_df
+
+# we identify the last touch (last interaction with a marketing channel)
+# for every user_id and store that in this dataframe.
+def create_last_touch_df():
+    # sort all the clicks by the datetime in asc order
+    sorted_clicks_df = clicks.sort_values(by='click_datetime')
+    last_touch_df = sorted_clicks_df.drop_duplicates(subset=['user_id'], keep='last')
+
+    return last_touch_df
+
+def calculate_sales_per_channel_id():
+    # CHANGE THE CODE TO USE A DIFF DF FROM sales BY DROPPING SOME NULL USER_IDs
+    sales_per_user_id_df = sales.groupby('user_id').size().reset_index(name='no_of_sales')
+
+    # create first touch df by calling a function
+    first_touch_df = create_first_touch_df()
+
+    # map the sales for each user_id and attribute that to a channel based on the first touch
+    sales_per_user_id_merged_first_touch_df = pd.merge(sales_per_user_id_df, first_touch_df, on = 'user_id')
+
+    # count the sales for each channel id
+    sales_per_channel_id_df = pd.DataFrame(sales_per_user_id_merged_first_touch_df.groupby('channel_id')['no_of_sales'].sum())
+
+    return sales_per_channel_id_df
+
